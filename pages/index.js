796bf20b9a3a2fb2c48beff4a6ea7799f7de71f9
triplezero903 <triplezero903@hotmail.com>
2022-10-13 12:28:45 +0900

    3-List product

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,14 @@
-import Head from "next/head";
 import Layout from "../components/Layout";
-import styles from "../styles/Home.module.css";
+import ProductItems from "../components/ProductItems";
+import data from "../utils/data";
 
 export default function Home() {
   return (
-    <Layout title="홈페이지">
-      <div className="">
-        <h1 className="text-3xl font-bold">Tailwind CSS</h1>
-        <p>웹서버프로그래밍, 손경현.</p>
+    <Layout title="Home">
+      <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6">
+        {data.products.map((product) => (
+          <ProductItems product={product} key={product.slug} />
+        ))}
       </div>
     </Layout>
   );
