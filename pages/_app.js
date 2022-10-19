fbc64aa2e70f84dff0ad8ce281fb036474792b5a
triplezero903 <triplezero903@hotmail.com>
2022-10-19 17:47:54 +0900

    modified_2

---
+++ pages/_app.js
@@ -0,0 +1,10 @@
+import "../styles/globals.css";
+import { StoreProvider } from "../utils/Store";
+function MyApp({ Component, pageProps }) {
+  return (
+    <StoreProvider>
+      <Component {...pageProps} />
+    </StoreProvider>
+  );
+}
+export default MyApp;
