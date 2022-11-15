8f309d1c2472759fc4980fec6e7e0f422c5b1333
triplezero903 <triplezero903@hotmail.com>
2022-11-15 10:32:55 +0900

    15-SeedSampleProduct

--- pages/api/seed.js
+++ pages/api/seed.js
@@ -1,3 +1,4 @@
+import Product from "../../models/Product";
 import User from "../../models/User";
 import data from "../../utils/data";
 import db from "../../utils/db";
@@ -6,6 +7,8 @@ const handler = async (req, res) => {
   await db.connect();
   await User.deleteMany();
   await User.insertMany(data.users);
+  await Product.deleteMany();
+  await Product.insertMany(data.products);
   await db.disconnect();
   res.send({
     message: "seeded successfully... 초기 사용자가 성공적으로 등록되었습니다!.",
